44e98c480f5df6553ea969a9c2b9a3adee97be66
zazayudaperwira <52827975+zazayudaperwira@users.noreply.github.com>
2022-01-17 09:56:28 +0700

    change to android ver

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -21,21 +21,21 @@ class GuestController extends Controller
     {
         
         //Untuk di run di Android
-        // $folderPath = public_path('/images/ttd/ttd1');
-        // $image_parts = explode(";base64,", $request->signed);
-        // $image_type_aux = explode("image/", $image_parts[0]);
-        // $image_base64 = base64_decode($image_parts[0]);
-        // $file = $folderPath . uniqid() . '.png';
-        // file_put_contents($file,$image_base64);
+         $folderPath = public_path('/images/ttd/ttd1');
+         $image_parts = explode(";base64,", $request->signed);
+         $image_type_aux = explode("image/", $image_parts[0]);
+         $image_base64 = base64_decode($image_parts[0]);
+         $file = $folderPath . uniqid() . '.png';
+         file_put_contents($file,$image_base64);
       
         // Untuk Di run Di PC
-        $folderPath = public_path('/images/ttd/ttd1');
-        $image_parts = explode(";base64,", $request->signed);
-        $image_type_aux = explode("image/", $image_parts[0]);
-        $image_type = 'png';
-        $image_base64 = base64_decode($image_parts[1]);
-        $file = $folderPath . uniqid() . '.' . $image_type;
-        file_put_contents($file, $image_base64);
+        //$folderPath = public_path('/images/ttd/ttd1');
+        //$image_parts = explode(";base64,", $request->signed);
+        //$image_type_aux = explode("image/", $image_parts[0]);
+        //$image_type = 'png';
+        //$image_base64 = base64_decode($image_parts[1]);
+        //$file = $folderPath . uniqid() . '.' . $image_type;
+        //file_put_contents($file, $image_base64);
 
         $data = $request->validate([
             'name' => ['required', 'max:255'],
